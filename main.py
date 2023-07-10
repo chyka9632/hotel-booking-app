f4dda28a11522ab0d9dbebaeb8770103e6a80b03
Chika <chyka9632@gmail.com>
2023-07-10 22:27:14 +0100

    Add credit card features - Extend the code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv("hotels.csv")
+df_cards = pd.read_csv("cards.csv").to_dict(orient="records")
 
 
 class Hotel:
@@ -37,14 +38,34 @@ class ReservationTicket:
         return content
 
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, expiration, holder_name, cvc):
+        card_data = {"number": self.number, "expiration": expiration,
+                     "holder": holder_name, "cvc": cvc}
+        if card_data in df_cards:
+            return True
+
+
 print(df)
 hotel_ID = int(input("Enter the id of the hotel: "))
 hotel = Hotel(hotel_ID)
 
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(customer_name=name, hotel_name=hotel.name)
-    print(reservation_ticket.generate())
+    number = int(input("Enter the 16 digit number in front of your card: "))
+    expiration = input("Enter the expiry date of your card: ")
+    cvc = int(input("Enter the 3 digit number on the back of your card: "))
+    holder = input("Enter your name as it appears on the card: ")
+    credit_card = CreditCard(number=number)
+    if credit_card.validate(expiration=expiration, cvc=cvc,
+                            holder_name=holder):
+        hotel.book()
+        name = input("Enter your name: ")
+        reservation_ticket = ReservationTicket(customer_name=name, hotel_name=hotel.name)
+        print(reservation_ticket.generate())
+    else:
+        print("Couldn't validate your card, review your card details or try another card")
 else:
-    print("Hotel is not available")
+    print("Hotel is unavailable")
